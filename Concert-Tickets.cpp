c82b584f0dffb0aec1bbbe1cb60d13fa90c39f3e
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2021-04-19 08:49:54 +0800

    s

--- Concert-Tickets.cpp
+++ Concert-Tickets.cpp
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-long long h[200005], t[200005];
+long long h, t[200005];
+multiset<long long, greater<long long>> se;
 int main(){
     long long n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; i++)
-        cin >> t[t];
-    for(int i = 0; i < m; i++)
-        cin >> h[i];
-    sort()
+    for(int i = 0; i < n; i++){
+		long long a;
+		cin >> a;
+		se.insert(a);
+	}
+	
+    for(int i = 0; i < m; i++){
+        cin >> h;
+		auto it = se.lower_bound(h);
+		if(it == se.end()){ cout << -1 << '\n'; continue;}
+		else {
+			cout << *it << '\n';
+			se.erase(it);
+		}
+	}
 }
