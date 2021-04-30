e122e879cefdc0582805f9ff44aaf24ac2103bd9
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2021-05-01 02:22:02 +0800

    a

--- Static-Range-Sum-Queries.cpp
+++ Static-Range-Sum-Queries.cpp
@@ -2,36 +2,43 @@
 
 using namespace std;
 
-int tree[800000];
-int arr[200000];
-void build(int index, int l, int r) {
-    if(l == r){ tree[index] = arr[l]; return;}
-    int m = (l + r) / 2;
-    build(index*2, l, m);
-    build(index * 2 + 1, m + 1, r);
-    tree[index] = tree[index * 2] + tree[index * 2 + 1];
+const int MAX = 2e5 + 5;
+
+long long tree[4 * MAX];
+long long arr[MAX];
+
+
+void build(long long index, long long l, long long r){
+	if(l == r){
+		tree[index] = arr[l];
+		return;
+	}
+	long long m = (l + r) / 2;
+	build(index * 2, l, m);
+	build(index * 2 + 1, m + 1, r);
+	tree[index] = tree[index * 2] + tree[index * 2 + 1];
+	return;
 }
 
-int query(int ql, int qr, int index, int l, int r){
-    if(ql <= l && r <= qr) return tree[index];
-    int m = (l + r) / 2;
-    if(ql > m) {
-        return query(ql, qr, index * 2 + 1, m + 1, r);
-    }
-    if(qr <= m) {
-        return query(ql, qr, index * 2, l, m);
-    }
-    return query(ql, qr, index * 2, l, m) + query(ql, qr, index * 2 + 1, m + 1, r);
+long long query(long long ql, long long qr, long long index, long long l, long long r) {
+	if(ql <= l && r <= qr) return tree[index];
+	long long m = (l + r) / 2;
+	if(ql > m) return query(ql, qr, index * 2 + 1, m + 1, r);
+	if(qr <= m) return query(ql, qr, index * 2, l, m);
+	return query(ql, qr, index * 2, l, m) + query(ql, qr, index * 2 + 1, m + 1, r);
 }
 
 int main(){
-    int n, q;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    long long n, q;
     cin >> n >> q;
     for(int i = 0; i < n; i++)
         cin >> arr[i];
     build(1, 0, n - 1);
-    for(int i = 0; i < q; i++){
-        int a, b;
-        cout << query(a, b - 1, 1, 0, n - 1) << '\n';
-    }
+	for(int i = 0; i < q; i++){
+		long long a, b;
+		cin >> a >> b;
+		cout << query(a, b-1, 1, 0, n - 1 ) << '\n';
+	}
 }
---
+++ Static-Range-Sum-QueriesRMQ.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int MAX = 2e5 + 5;
+int rmq[MAX][50];
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++)
+		cin >> rmq[i][0];
+	for(int j = 1; (1 << j) <= n; ++j)
+		for(int i = 1; i + (1 << j)-1 <= n; ++i)
+			rmq[i][j] = max(rmq[i][j - 1], rmq[i + (1 << (j - 1))][j - 1]);
+	for(int i = 0; i < m; i++){
+		int a, b;
+		cin >> a >> b;
+		int k = log2(b - a + 1);
+		cout << max(rmq[a][k], rmq[b - (1 << k) + 1][k]) << '\n';
+	}
+}
