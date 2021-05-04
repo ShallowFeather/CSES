af7f9bfa2f2313dc7cab77e5fb6124756f8e3cc1
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2021-05-05 00:49:08 +0800

    a

--- Dynamic-Range-Minimum-Queries.cpp
+++ Dynamic-Range-Minimum-Queries.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 const int MAX = 2e5 + 5;
 long long arr[MAX], tree[4 * MAX];
 
-void build(int index, int l, int r) {
+void build(int index, int l, int r){
 	if(l == r) {
 		tree[index] = arr[l];
 		return;
@@ -12,33 +13,31 @@ void build(int index, int l, int r) {
 	int m = (l + r) / 2;
 	build(index * 2, l, m);
 	build(index * 2 + 1, m + 1, r);
-	tree[index] = tree[index * 2] + tree[index * 2 + 1];
-}
+	tree[index] = min(tree[index * 2], tree[index * 2 + 1]);
+} 
 
-long long query(int ql, int qr, int index, int l, int r) {
-	if(ql <= l && r <= qr){
-		return tree[index];
-	}
-	int m = (l + r) / 2;
-	if(qr <= m) return query(ql, qr, index * 2, l, m);
-	if(ql > m) return query(ql, qr, index * 2 + 1, m + 1, r);
-	return query(ql, qr, index * 2, l, m) + query(ql, qr, index * 2 + 1, m + 1, r);
-}
-
-void update(int pos, int val, int index,int l,int  r){
-	if(l == r) {
+void update(int pos, int val, int index, int l, int r){
+	if(l == r){
 		tree[index] = val;
 		return;
 	}
 	int m = (l + r) / 2;
 	if(pos <= m) update(pos, val, index * 2, l, m);
 	else update(pos, val, index * 2 + 1, m + 1, r);
-	tree[index] = (tree[index * 2]  + tree[index * 2 + 1]);
+	tree[index] = min(tree[index * 2], tree[index * 2 + 1]);
+}
+
+long long query(int ql, int qr, int index, int l, int r) {
+	if(ql <= l && r <= qr){
+		return tree[index];
+	}
+	int m =(l + r) / 2;
+	if(qr <= m) return query(ql, qr, index * 2, l, m);
+	if(ql > m) return query(ql, qr, index * 2 + 1, m + 1, r);
+	return min(query(ql, qr, index * 2, l, m), query(ql, qr, index * 2 + 1, m + 1, r));
 }
 
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
 	int n, q;
 	cin >> n >> q;
 	for(int i = 0; i < n; i++)
@@ -48,9 +47,9 @@ int main(){
 		int op, a, b;
 		cin >> op >> a >> b;
 		if(op == 1){
-			update(a - 1, b, 1, 0, n - 1);
+			update(a - 1, b, 1, 0, n - 1);		
 		}
-		if(op == 2){
+		else {
 			cout << query(a - 1, b - 1, 1, 0, n - 1) << '\n';
 		}
 	}
